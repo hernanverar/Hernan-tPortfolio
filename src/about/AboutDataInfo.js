69b3e70b80f4e3711ed99d4b3d5c51e9ea5a5a94
Hernan verar <hernanverar@gmail.com>
2024-01-24 14:20:11 -0800

    this commit adds all our about.js and aboout.css files, also adds our images, background, and layout of our routes

---
+++ src/about/AboutDataInfo.js
@@ -0,0 +1,75 @@
+const subheadingsData = {
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+          I am a highly motivated and personable Junior Full Stack Developer with a strong skill set. My passion for technology drives my eagerness to contribute to innovative projects within a collaborative environment.
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "Software Engineer",
+      content: (
+        <>
+          <p>
+            
+Epicodus Jan 2023 - Dec 2023
+<p>Computer Software Engineer
+bootcamp full stack frontend and backend web development including.
+</p> 
+HTML, Javascript, CSS, Bootstrap, C#, SQL and React. Coding is performed by working in class with a different pair every day and solo projects on Fridays.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "Graduate",
+      content: (
+        <>
+          <p>
+            Epicodus graduate.
+          </p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "Student",
+      content: (
+        <>
+          <p>
+            Student
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "Constructor",
+      content: (
+        <>
+          <p>
+            constructor
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "labor",
+      content: (
+        <>
+          <p>
+            Labor
+          </p>
+        </>
+      ),
+    },
+  ],
+};
+
+export default subheadingsData;
---
+++ src/avatar/Avatar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import avatarImage from "../assets/Hernan.jpeg";
+import "../styles/avatar.css";
+
+const Avatar = ({ page }) => {
+  const avatarClass = `avatar ${page}`;
+
+  return (
+    <>
+
+      <img src={avatarImage} className={avatarClass} alt="avatar" />
+    </>
+  );
+};
+
+Avatar.propTypes = {
+  page: PropTypes.string.isRequired,
+};
+
+export default Avatar;
