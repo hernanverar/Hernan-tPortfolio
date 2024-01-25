072d15cc48aa3df57995fa2098eddee8fbd34d68
Hernan verar <hernanverar@gmail.com>
2024-01-24 16:51:41 -0800

    this commit adds myStats folders and CSS files to our app

---
+++ src/myStats/HernanStats.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/hernanStats.css";
+
+const HernanStats = () => {
+  return (
+    <div id="HernanStats">
+      <h1>HERNAN VERAR </h1>
+      <div id="playerStats__lines">
+        <span id="HernanStats__lines__thick"></span>
+        <span id="HernanStats__lines__thin"></span>
+      </div>
+      <h2>Fullstack Developer</h2>
+    </div>
+  );
+};
+
+export default HernanStats;
